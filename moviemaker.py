52f1ed2c0901841ee86c673bfb0f5cd80f664b9e
Yannick Badoux <yannick@casema.nl>
2025-01-06 15:47:08 +0100

    added support for more efficient video codec

--- moviemaker.py
+++ moviemaker.py
@@ -10,7 +10,7 @@ import glob
 import argparse
 
 
-def moviemaker(image_folder, video_name, fps=10, height=1080, width=1440):
+def moviemaker(image_folder, video_name, fps=10, height=1080, width=1440, codec='XVID'):
 
     if not os.path.isdir(image_folder):
         sys.exit(f'{image_folder} is not a folder. Quitting.')
@@ -21,8 +21,13 @@ def moviemaker(image_folder, video_name, fps=10, height=1080, width=1440):
     image_numbers = [image_name.split('-')[-1].split('.')[0] for image_name in unsorted_images]
     image_numbers = list(map(int, image_numbers))
     _, sorted_images = zip(*sorted(zip(image_numbers, unsorted_images)))
+    print(len(sorted_images))
 
-    video = cv2.VideoWriter(video_name, 0, fps, (width, height))
+    fourcc = cv2.VideoWriter_fourcc(*codec)
+    video = cv2.VideoWriter(video_name, fourcc, fps, (width, height))
+
+    if not video.isOpened():
+        print('dinges')
 
     for image_name in sorted_images:
         img = cv2.imread(image_name)
@@ -40,6 +45,7 @@ if __name__ == '__main__':
     parser.add_argument('--fps', default=10, type=int, help='Frames per second of the video.')
     parser.add_argument('--height', default=1080, type=int, help='Height of the video.')
     parser.add_argument('--width', default=1440, type=int, help='Width of the video.')
+    parser.add_argument('--codec', default='XVID', type=str, help='Video codec used by OpenVC VideoWriter. (Recommend: avc1 or XVID)')
     args = parser.parse_args()
 
     video_name = args.video_name
@@ -47,5 +53,6 @@ if __name__ == '__main__':
     fps = args.fps
     height = args.height
     width = args.width
+    codec = args.codec
 
-    moviemaker(image_folder, video_name, fps, height, width)
+    moviemaker(image_folder, video_name, fps, height, width, codec)
